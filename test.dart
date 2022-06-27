c934a9d109e88d3822fe206c28f73e72ed03ac4d
dabuzida <dabuzida@gmail.com>
2022-06-27 18:08:23 +0900

    1

--- test.dart
+++ test.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
-void main() {
+void main() async {
+  print(Random().nextInt(10));
+  await Future.delayed(Duration(seconds: 2), () => print(Random().nextInt(10)));
   print(Random().nextInt(10));
 }
