2d03bed53c7440f1dba6782c475b7d70548498e0
dabuzida <dabuzida@gmail.com>
2022-07-14 18:05:31 +0900

    test

--- test.dart
+++ test.dart
@@ -9,4 +9,19 @@ void main() async {
   int y = x ~/ 1000;
   print(x);
   print(y);
+
+  List xx = [];
+  if (xx is List) {
+    print(1);
+  }
+  if (true && 'asd' != null && true) {
+    // if (true && 'asd' == null) {
+    print(328938939);
+  }
+
+  // widget.showPublishMenu && (hasValidData != null && hasValidData!) ? _buildUIPublishMenu() : SizedBox.shrink(),
+  bool? k;
+  if (k != null && ) {
+    print(49494);
+  }
 }
