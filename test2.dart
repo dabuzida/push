8dc6b2f56de83c812c431743408cb2c4f2ac8187
dabuzida <dabuzida@gmail.com>
2022-07-20 18:05:02 +0900

    1

---
+++ test2.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+void main() {
+  Map x = {
+    'a': 'aaa',
+    // 'b': 'bbb',
+    // 'c': 'ccc',
+    // 'd': 'ddd',
+  };
+  // print(x['a']);
+  // print(x['d']);
+  // print(x['e']);
+  // print(x['f']);
+  print(x.keys);
+  print(x.keys.runtimeType);
+  // print(x.keys.elementAt(0));
+}
