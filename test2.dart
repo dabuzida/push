de0bff8067bbc886ba7d4f8a52e95329b1d3d4e9
dabuzida <dabuzida@gmail.com>
2022-08-01 17:57:40 +0900

    1

--- test2.dart
+++ test2.dart
@@ -1,28 +1,7 @@
 import 'dart:math';
 
 void main() {
-  print(x());
-  print(x2());
-  x3();
-  x4();
-  x5();
-  x7();
+  final x = [1, 2, 3, 4, 5];
+  final y = x.reversed.toList();
+  print(y);
 }
-
-int x() => 11;
-int x2() {
-  return 12;
-}
-
-void x3() => 13;
-void x4() {
-  print(14);
-}
-
-void x5() => x4();
-int x6() {
-  print(10000);
-  return 15;
-}
-
-void x7() => x6();
