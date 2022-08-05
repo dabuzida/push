44f0a69b125df9e95d8c9f602b6a441a32433e52
dabuzida <dabuzida@gmail.com>
2022-08-05 18:03:39 +0900

    1

--- test2.dart
+++ test2.dart
@@ -4,4 +4,16 @@ void main() {
   final x = [1, 2, 3, 4, 5];
   final y = x.reversed.toList();
   print(y);
+  final func = test;
+  final func2 = test2;
+
+  Future.delayed(Duration(seconds: 2), () => func());
+}
+
+void test() {
+  print(10);
+}
+
+int test2() {
+  return 101;
 }
