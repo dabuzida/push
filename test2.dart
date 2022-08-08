470ce848b91f02f6356915e5faa3f763c43d5c23
dabuzida <dabuzida@gmail.com>
2022-08-08 18:03:12 +0900

    1

--- test2.dart
+++ test2.dart
@@ -1,19 +1,11 @@
 import 'dart:math';
 
 void main() {
-  final x = [1, 2, 3, 4, 5];
-  final y = x.reversed.toList();
-  print(y);
-  final func = test;
-  final func2 = test2;
-
-  Future.delayed(Duration(seconds: 2), () => func());
+  print(1);
+  x();
+  print(2);
 }
 
-void test() {
-  print(10);
-}
-
-int test2() {
-  return 101;
+void x() {
+  return;
 }
