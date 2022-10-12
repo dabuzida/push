966185d75a20fc485113faf99353388fbd86364d
dabuzida <dabuzida@gmail.com>
2022-10-12 17:04:23 +0900

    전화번호생성

--- test4.dart
+++ test4.dart
@@ -1,16 +1,26 @@
 import 'dart:math';
 
 void main() {
-  num a = 11;
-  num b = 11.343;
-  print(a / 11);
-  print(b / 3);
+  // num a = 11;
+  // num b = 11.343;
+  // print(a / 11);
+  // print(b / 3);
 
-  int w = 16420;
-  print(w ~/ 3600);
-  print(w % 3600);
+  // int w = 16420;
+  // print(w ~/ 3600);
+  // print(w % 3600);
 
-  // String x = ' ';
-  // int xx = int.parse(x);
-  // print(xx);
+  for (int i = 0; i < 3000; i++) {
+    double num = Random().nextDouble();
+    // print(Random().nextInt(2));
+    String s = '010';
+    if (num > 0.09) {
+      // print(num);
+      int n = (num * 100000000).ceil();
+      // print((num * 100000000).ceil());
+      s += n.toString();
+      print(s);
+      // print((num * 100000000000).ceil());
+    }
+  }
 }
