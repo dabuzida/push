1dc8ff9cefd0b8cc7df4aad5d8120156efad7e4b
dabuzida <dabuzida@gmail.com>
2023-06-22 13:46:07 +0900

    test

--- sort.dart
+++ sort.dart
@@ -2,10 +2,15 @@ void main() {
   final List<String> ko = <String>['강아지', '악어', '코끼리', '독수리'];
   final List<String> en = <String>['dog', 'cat', 'whale', 'eagle'];
 
-  print(ko);
-  print(en);
-  ko.sort((a, b) => a.compareTo(b));
-  en.sort((a, b) => a.compareTo(b));
-  print(ko);
-  print(en);
+  // print(ko);
+  // print(en);
+  // ko.sort((a, b) => a.compareTo(b));
+  // en.sort((a, b) => a.compareTo(b));
+  // print(ko);
+  // print(en);
+
+  final List<int> xx = <int>[2, 5, 2, 6, 34, 756, 1];
+  print(xx);
+  xx.sort((a, b) => a.compareTo(b));
+  print(xx);
 }
--- test8.dart
+++ test8.dart
@@ -1,8 +1,11 @@
 void main() {
-  print('start');
+  // print('start');
 
-  _xx();
-  print('finish');
+  // _xx();
+  // print('finish');
+
+  print(AlzWINTestClass.call.name);
+  print(AlzWINTestClass.call.name.runtimeType);
 }
 
 void _xx() {
@@ -11,4 +14,24 @@ void _xx() {
     print(i);
   }
   print('###');
+}
+
+enum AlzWINTestClass {
+  call('전화'), // 전화검사, 개인정보동의
+  screen('앱/웹'), // 문자링크, (회원, 비회원) * (바로시작, 링크생성)
+  upload('업로드'), // 키오스크, 공개검사링크
+  ;
+
+  const AlzWINTestClass(this.serverValue);
+
+  final String serverValue;
+
+  // String get translationKey {
+  //   switch (this) {
+  //     case required:
+  //       return 'required';
+  //     case optional:
+  //       return 'optional';
+  //   }
+  // }
 }
