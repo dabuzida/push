f64c2a8746c83f4d538b716e181cbb2264711145
dabuzida <dabuzida@gmail.com>
2023-07-28 18:01:15 +0900

    test

--- date_time5.dart
+++ date_time5.dart
@@ -1,41 +1,19 @@
 void main() {
-  // final DateTime now = DateTime.now(); // 2022-12-27 10:22:01.619368
-  // print(now);
-  // print(now.year);
-  // print(now.year.runtimeType);
-
-  // final DateTime x = DateTime(2022, 11, 3);
-  // print(x);
-  // final int xx = x.millisecondsSinceEpoch;
-  // print(xx);
-  // DateTime z = DateTime.fromMillisecondsSinceEpoch(xx);
-  // print(z);
-
-  final String ss = '1122';
-  final String rr = '112';
-  print(ss == rr);
-  print(identical(ss, rr));
-  print(ss.compareTo(rr));
-
-  final List<String> list = <String>[
-    '2022-11-18 11:22',
-    '2022-11-17 11:22',
-  ];
-
-  final String v1 = list[0];
-  final String v2 = list[1];
-  final String d1 = '2022-11-18';
-  final String d2 = '2022-11-17';
-  final String d3 = v1.substring(0, 10);
-  final String d4 = v2.substring(0, 10);
-
-  // print(d3);
-  // print(d4);
-  // print(d1 == d2);
-  // print(identical(d1, d2));
-  // print(d1.compareTo(d2));
-  // print('====');
   // print(d3 == d4);
   // print(identical(d3, d4));
   // print(d3.compareTo(d4));
+  Count xx = Count.one;
+  print(xx.value);
+}
+
+enum Count {
+  zero('00'),
+  one('01'),
+  two('02'),
+  three('03'),
+  ;
+
+  const Count(this.value);
+
+  final String value;
 }
